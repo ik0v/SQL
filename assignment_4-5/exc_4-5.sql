40e366d93ca000c5863756a60520fac1add2c74b
Ivan Koval <ik@hotmail.com>
2020-11-06 21:15:47 +0100

    added excercise 5

--- assignment_4-5/exc_4-5.sql
+++ assignment_4-5/exc_4-5.sql
@@ -23,10 +23,42 @@ SELECT name
 FROM world 
 WHERE name LIKE 'Ne%' OR name LIKE 'No%';
 
-1.5 Get all countries with population between 4 and 6 millions.
+# 1.5 Get all countries with population between 4 and 6 millions.
 
 SELECT * FROM world 
 WHERE population BETWEEN 4000000 AND 6000000;
+
+# 1.6 Count all countries in table
+SELECT COUNT(*) AS nr_countries 
+FROM world;
+
+# 1.7 Count countries with populatione between 4 and 6 millions.
+
+SELECT COUNT(*) AS nr_countries
+FROM world
+WHERE population 
+BETWEEN 4000000 AND 6000000;
+
+# 1.8 Get average population per square km in whole Europe.
+
+SELECT ROUND(AVG(population/area),2) 
+AS 'pop density in Europe'
+FROM world
+WHERE continent = 'Europe';
+
+# 1.9 Count nr countries for each continent.
+
+SELECT continent, COUNT(*) AS 'nr countries'
+FROM world
+GROUP BY continent;
+
+# 1.10 Get continents with more than 10 countries.
+
+SELECT continent, COUNT(*) AS 'nr countries' 
+FROM world
+GROUP BY continent
+HAVING COUNT(*) >10;
+
 
 
 
